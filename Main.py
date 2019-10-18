9fbb6c39ccaf3a18c8008bdee22f4ea5b6f6a339
Vitor Henrique Moreira <vhsouzaeng@gmail.com>
2019-10-18 09:39:54 -0300

    users list add

--- Main.py
+++ Main.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template, request, redirect
+import json
+#from mysql import connector
 from pymongo import MongoClient
 
+
 app = Flask(__name__)
-cliente = MongoClient('{{ YOUR CONNECT KEY MONGODB }}')
+
+#db = connector.connect(host='localhost',user='adm',passwd='Azaleia150')
+
+cliente = MongoClient('mongodb://127.0.0.1:27017')
 db = cliente['db']
 
 @app.route('/',methods=['GET'])
@@ -15,11 +21,16 @@ def armazenar():
     email = request.form['email']
     senha = request.form['senha']
 
+
+
     user = db['user']
     post = {"name":name,"email":email,"senha":senha}
 
     user.insert_one(post)
+    lista = []
+    for u in user.find():
+        lista.append(u)
     
-    return redirect('/')
+    return render_template('List.html', list=lista)
 
 app.run()
